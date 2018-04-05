54cb7c9ad0a6085b75cc8ef344e0302de074fbcd
Loic <loicvangaver@gmail.com>
2018-04-05 15:49:12 -0400

    Update

---
+++ find-supplier-cp.sh
@@ -0,0 +1,20 @@
+Motovan
+cd /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/download && unzip MOTOVAN_EXT_FR.csv.zip && cp MOTOVAN_EXT_FR.csv /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/Motovan.csv
+
+Klim
+cd /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/download && cp Canada Inventory.csv /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/Klim.csv
+
+Choko
+cd /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/download && cp CDI-ATS.csv /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/Choko.csv
+
+ITL
+cd /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/download && cp itl4.csv /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/Itl.csv
+
+Kimpex
+cd /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/download && unzip 5274489.zip && cp /5274489/5274489_price_list.txt /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/Kimpex.txt
+
+Gamma
+cd /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/download/PRICE\ FILES && find . -name \*.csv -exec cp {} /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/Gamma.csv \;
+
+Forma
+cd /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/download/depot && find . -name '1.csv' -maxdepth 2 -mtime -1 -exec cp {} /Users/loicvangaver/Documents/ADM\ Sport/webmaster/link-fournisseur/Forma.csv \;
